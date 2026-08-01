a4350a6896f40d0c3279ba26bb7c30a40d78239e
lakshayui <lakshaytyagi897@gmail.com>
2026-07-31 20:29:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".watch-now").addEventListener("click",()=>{
+    window.open("https://youtu.be/wIynl3at0Rs?si=NTgBEbrmRDjbjFeU","_blank");
+});
+
+document.querySelector("#play-button").addEventListener("click",()=>{
+    window.open("https://youtu.be/wIynl3at0Rs?si=NTgBEbrmRDjbjFeU","_blank");
+});
+
+document.querySelector("#signup-btn").addEventListener("click",()=>{
+    window.open("form.html","_blank");
+});
+
+document.querySelector(".get-started").addEventListener("click",()=>{
+    window.open("form.html","_blank");
+});
